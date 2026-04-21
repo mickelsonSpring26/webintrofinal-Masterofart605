ff1f6d6ac0e7a13756cfbefec48d8d3b6c28c2b6
unknown <ben.seamons@ENGINEERING.SNOW>
2026-04-21 11:49:13 -0600

    test renderdotcom

--- docs/service.js
+++ docs/service.js
@@ -1,23 +1,26 @@
+// const baseUrl = `http://localhost:5050`;
+const baseUrl = `https://webintrofinal-masterofart605.onrender.com`
+
 export const GetFullList = async () => {
-  const responce = await fetch("http://localhost:5050/fullList");
+  const responce = await fetch(`${baseUrl}/fullList`);
   const data = await responce.json();
   return data;
 };
 
 export const GetRandom = async () => {
-  const responce = await fetch("http://localhost:5050/random");
+  const responce = await fetch(`${baseUrl}/random`);
   const data = await responce.json();
   return data;
 };
 
 export const GetRandomKitless = async () => {
-  const responce = await fetch("http://localhost:5050/randomKitless");
+  const responce = await fetch(`${baseUrl}/randomKitless`);
   const data = await responce.json();
   return data;
 };
 
 export const SendListData = async (data) =>{
-  await fetch("http://localhost:5050/customList", {
+  await fetch(`${baseUrl}/customList`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,7 +29,7 @@ export const SendListData = async (data) =>{
   });
 }
 export const SendUsername = async (data) =>{
-  await fetch("http://localhost:5050/name", {
+  await fetch(`${baseUrl}/name`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
